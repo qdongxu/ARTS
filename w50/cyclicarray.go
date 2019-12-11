2b133727f7681e3972ea76734323b941523c75ee
Xu Dongyan <qdongxu9@gmail.com>
2019-12-11 11:20:18 +0800

    add fixed size feature

--- w50/cyclicarray.go
+++ w50/cyclicarray.go
@@ -1,6 +1,9 @@
 package codekata
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 const (
 	initCapacity = 8
@@ -18,67 +21,136 @@ type (
 		head     int
 		tail     int
 		size     int
+		maxSize  int
+
+		lock *sync.Mutex
 	}
 )
-func NewCyclicArray() *CyclicArray {
-	return &CyclicArray{capacity: initCapacity, array: make([]*Data, initCapacity)}
+
+// NewCyclicArray creates and returns a new cyclic array.
+//
+// the length will be fixed to maxSize if maxSize is a non zero value. and Push will overwrite the earliest element
+// when beyond one circle.
+//
+// the length will be extended automatically if maxSize is 0
+//
+// Note: maxSize should not be less than 0
+//
+func NewCyclicArray(maxSize int) *CyclicArray {
+	if maxSize != 0 {
+		return &CyclicArray{capacity: maxSize, maxSize: maxSize, array: make([]*Data, maxSize), lock: &sync.Mutex{}}
+	}
+
+	return &CyclicArray{capacity: initCapacity, maxSize: maxSize, array: make([]*Data, initCapacity), lock: &sync.Mutex{}}
 }
 
 func (c *CyclicArray) Get(index int) *Data {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	realPos := c.index2RealPos(index)
+	return c.array[realPos]
+}
+
+func (c *CyclicArray) index2RealPos(index int) int {
 	if index >= c.size {
 		panic(fmt.Sprintf("out of index: index: %d, size: %d", index, c.size))
 	}
-
 	index += c.head
 	if index >= c.capacity {
 		index -= c.capacity
 	}
-	return c.array[index]
+	return index
 }
 
 func (c *CyclicArray) Set(index int, data *Data) {
-	index += c.head
-	if index >= c.capacity {
-		index -= c.capacity
-	}
+	c.lock.Lock()
+	defer c.lock.Unlock()
 
-	c.array[index] = data
+	realPos := c.index2RealPos(index)
+
+	c.array[realPos] = data
 }
 
 func (c *CyclicArray) Size() int {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
 	return c.size
 }
 
 func (c *CyclicArray) Push(data *Data) {
-	if c.size >= c.capacity {
-		fmt.Printf("resise: %d\n", c.capacity)
-		newArray := make([]*Data, c.capacity*2)
-		for i := 0; i < c.size; i++ {
-			newArray[i] = c.Get(i)
-		}
+	c.lock.Lock()
+	defer c.lock.Unlock()
 
-		c.array = newArray
-		c.capacity *= 2
-		c.head = 0
-		c.tail = c.size
-
+	if c.isAutoExtend() && c.size >= c.capacity {
+		c.extendSize()
 	}
 
-	c.size++
+	if c.size < c.capacity {
+		c.array[c.tail] = data
+		c.tail++
+		c.size++
+		if c.tail >= c.capacity {
+			c.tail = 0
+		}
+
+		return
+	}
+
+	// overwrite the head element as the fixed length array is full
+	c.head++
+	if c.head >= c.capacity {
+		c.head = 0
+	}
 
 	c.array[c.tail] = data
 	c.tail++
 	if c.tail >= c.capacity {
 		c.tail = 0
 	}
+
 }
 
-func (c *CyclicArray) Take() *Data {
-	if c.size < 1 {
-		return nil
+func (c *CyclicArray) isAutoExtend() bool {
+	return c.maxSize <= 0
+}
+
+func (c *CyclicArray) extendSize() {
+	fmt.Printf("resise: %d\n", c.capacity)
+	newArray := make([]*Data, c.capacity*2)
+
+	if c.tail > c.head {
+		for i, j := 0, c.head; j < c.tail; i, j = i+1, j+1 {
+			newArray[i] = c.array[j]
+		}
+
+	} else {
+		i := 0
+		for j := c.head; j < c.capacity; i, j = i+1, j+1 {
+			newArray[i] = c.array[j]
+		}
+
+		for j := 0; j < c.tail; i, j = i+1, j+1 {
+			newArray[i] = c.array[j]
+		}
 	}
 
-	rel := c.array[c.head]
+	c.array = newArray
+	c.capacity *= 2
+	c.head = 0
+	c.tail = c.size
+}
+
+func (c *CyclicArray) Take() (rel *Data, ok bool) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	if c.size < 1 {
+		return nil, false
+	}
+
+	rel = c.array[c.head]
 	c.array[c.head] = nil
 
 	c.size--
@@ -87,5 +159,5 @@ func (c *CyclicArray) Take() *Data {
 		c.head = 0
 	}
 
-	return rel
+	return rel, ok
 }
--- w50/cyclicarray_test.go
+++ w50/cyclicarray_test.go
@@ -1,38 +1,72 @@
 package codekata
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 )
 
 func TestCyclicArray(t *testing.T) {
-	array := NewCyclicArray()
-	for i := 0; i < 17; i++ {
+	array := NewCyclicArray(0)
+	size := 3498
+	for i := 0; i < size; i++ {
 		array.Push(&Data{Id: int32(i), Value: int32(i)})
 	}
 
-	for i := 0; i < rand.Intn(100)+7; i++ {
-		v := array.Take()
+	for i := 0; i < rand.Intn(1000)+7; i++ {
+		v, _ := array.Take()
 		array.Push(v)
 	}
 
-	for i := 0; i < 17; i++ {
-		fmt.Printf("%v, ", *array.Get(i))
+	for i := 0; i < size-1; i++ {
+		curr := *array.Get(i)
+		next := *array.Get(i + 1)
+		if next.Id == curr.Id+1 {
+			// OK
+		} else if next.Id == 0 {
+			// OK
+		} else {
+			t.Fatalf("elements out of order: No. %d(%v), No. %d(%v) ", i, curr, i+1, next)
+		}
+	}
+}
+
+func TestCyclicArrayFixLength(t *testing.T) {
+	maxSize := 10
+	array := NewCyclicArray(10)
+	size := 3498
+	for i := 0; i < size; i++ {
+		array.Push(&Data{Id: int32(i), Value: int32(i)})
 	}
 
+	small := size - maxSize
+
+	for i := 0; i < rand.Intn(100)+7; i++ {
+		v, _ := array.Take()
+		array.Push(v)
+	}
+
+	for i := 0; i < maxSize-1; i++ {
+		curr := *array.Get(i)
+		next := *array.Get(i + 1)
+		if next.Id == curr.Id+1 {
+			// OK
+		} else if next.Id == int32(small) {
+			// OK
+		} else {
+			t.Fatalf("elements out of order: No. %d(%v), No. %d(%v) ", i, curr, i+1, next)
+		}
+	}
 }
 
 func BenchmarkCyclicArray(b *testing.B) {
-	array := NewCyclicArray()
+	array := NewCyclicArray(0)
 	for i := 0; i < 17; i++ {
 		array.Push(&Data{Id: int32(i), Value: int32(i)})
 	}
 
-
 	for i := 0; i < b.N; i++ {
 		for i := 0; i < rand.Intn(100)+7; i++ {
-			v := array.Take()
+			v, _ := array.Take()
 			array.Push(v)
 		}
 	}
